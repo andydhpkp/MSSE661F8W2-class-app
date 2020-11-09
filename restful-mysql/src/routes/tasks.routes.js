7e71ed4697023072bbdb0c79f141b38e651fd5d1
andydhpkp <62804534+andydhpkp@users.noreply.github.com>
2020-11-08 22:38:35 -0700

    Update tasks.routes.js

--- restful-mysql/src/routes/tasks.routes.js
+++ restful-mysql/src/routes/tasks.routes.js
@@ -2,22 +2,12 @@ const controllers = require('../controllers/tasks.controller');
 const express = require('express');
 
 const tasksRoutes = express.Router();
-/**
- * Express routes for Tasks.
- *
- * RESTful endpoints make for easily adding to existing API features.
- */
 
-/**
- * Routes for all tasks. Evaluates to `/tasks/`.
- */
 tasksRoutes.get('/', controllers.getAllTasks).post('/', controllers.createTask);
 
-/**
- * Routes for a task by id. Evalutes to `/tasks/:taskId`.
- */
+
 tasksRoutes
-  .get('/:taskId', controllers.getTask) // GET http://locahost:3000/tasks/1
+  .get('/:taskId', controllers.getTask) 
   .put('/:taskId', controllers.updateTask)
   .delete('/:taskId', controllers.deleteTask);
 
